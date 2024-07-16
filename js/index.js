295f596e50095f95b8004550801437364073ab81
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-07-16 09:42:32 +0500

    small-time

---
+++ js/index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+    const time = document.querySelector("#time");
+    let date = new Date ();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let secound = date.getSeconds();
+    time.textContent = hours + " : " + minutes + " : " + secound + " PM "
+})  
